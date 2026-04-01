b783701ca7dd68e9fda07f42bc8fe9944c562f4a
root <root@ip-172-31-2-216.ap-south-1.compute.internal>
2026-04-01 12:57:37 +0000

    after pull commits

--- app.java
+++ app.java
@@ -47,9 +47,20 @@ public class AlarmReport {
         }
     }
 }
+
 // dev-2 code.
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+=======
+
+//dev-3 added code without pull 
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
     }
 }
